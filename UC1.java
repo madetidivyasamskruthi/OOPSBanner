94d6d76e092bda13cacf277c0705fb36bf8c8621
Madeti Divya Samskruthi <madetidivyasamskruthi@gmail.com>
2026-03-11 13:13:55 +0530

    UC1: Print OOPS to console

---
+++ UC1.java
@@ -0,0 +1,17 @@
+/**
+ * OOPS Banner App - Use Case 1
+ * Prints the literal text "OOPS" to the console.
+ *
+ * @author Madeti Divya
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        // Print the text OOPS
+        System.out.println("OOPS");
+
+    }
+}
